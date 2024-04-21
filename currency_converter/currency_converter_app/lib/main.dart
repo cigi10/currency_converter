e6028ee954b1b297845495751feffe8086664063
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2024-04-21 17:00:06 +0530

    added comments

--- currency_converter/currency_converter_app/lib/main.dart
+++ currency_converter/currency_converter_app/lib/main.dart
@@ -16,24 +16,28 @@ void main() {
 // 1. Material Design
 // 2. Cupertino Design
 
+// This is the main application widget for Material Design
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key); // Key is used to uniquely identify widgets
 
   @override
   Widget build(BuildContext context) {
+    // MaterialApp is a widget that provides Material Design specific functionality
     return const MaterialApp(
-      home: CurrencyConverterMaterialPage(),
+      home: CurrencyConverterMaterialPage(), // Setting the home page to CurrencyConverterMaterialPage
     );
   }
 }
 
+// This is the main application widget for Cupertino Design
 class MyCupertinoApp extends StatelessWidget {
-  const MyCupertinoApp({super.key});
+  const MyCupertinoApp({Key? key}) : super(key: key); // Key is used to uniquely identify widgets
 
   @override
   Widget build(BuildContext context) {
+    // CupertinoApp is a widget that provides Cupertino Design specific functionality
     return const CupertinoApp(
-      home: CurrencyConverterCupertinoPage(),
+      home: CurrencyConverterCupertinoPage(), // Setting the home page to CurrencyConverterCupertinoPage
     );
   }
 }
